f459cad014769cfab402eb5a28d0c03a830e0dcc
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-02-23 21:38:05 +0700

    initial project setup

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateAdmin(username: string, password: string) {
+    const admin = await this.prisma.admin.findUnique({
+      where: { username },
+    });
+
+    if (!admin) throw new UnauthorizedException('Invalid credentials');
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) throw new UnauthorizedException('Invalid credentials');
+
+    return admin;
+  }
+
+  async login(username: string, password: string) {
+    const admin = await this.validateAdmin(username, password);
+
+    const payload = {
+      sub: admin.id,
+      role: 'ADMIN',
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
