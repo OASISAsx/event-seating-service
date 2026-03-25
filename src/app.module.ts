31c20ddb40262ca129fde5b74b5c6644a67f4137
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-03-25 23:00:42 +0700

    update show image

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,16 @@ import { RegistrationModule } from './modules/registration/registration.module';
 import { EventsModule } from './modules/events/events.module';
 import { WebsocketGateway } from './websocket/websocket.gateway';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_URL!),
-
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
     UploadModule,
     WebsocketModule,
     AuthModule,
